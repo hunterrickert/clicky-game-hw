1d1c3606fb073f29f14e1c7217d7a393578d07bc
Hunter Rickert <hunter.rickert@gmail.com>
2019-09-25 14:29:30 -0700

    Complete

--- src/components/ImageClick/index.js
+++ src/components/ImageClick/index.js
@@ -4,11 +4,12 @@ import "./index.css";
 
 function ImageClick(props) {
     return (
-        <div onClick={() => props.handleImageClick(props.id)}>
+
+        <button onClick={() => props.handleImageClick(props.id)}>
             <img  alt="spongebob square pant is running"className="image-size" src={props.image} />
             {/* <p>{props.id}</p> */}
 
-        </div>
+        </button>
     )
 }
 
